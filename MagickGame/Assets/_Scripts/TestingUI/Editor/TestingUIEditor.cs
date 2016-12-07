dd74a60d83096ee3bdc47e32b10924e557644f66
JRueckert582 <jrueckert582@gmail.com>
2016-12-07 02:37:28 -0600

    added some custom editor UI
    added TestingUIScript to contain testing UI info as well as a scipt UI
    to edit
    and TestingUIEditor to edit the inspector GUI for TestingUIScript
    (allows for adding buttons,labels, and other GUI stuff to a script's
    inspector GUI)
    added TestingUI prefab to contain the testingUI script

---
+++ MagickGame/Assets/_Scripts/TestingUI/Editor/TestingUIEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TestingUIScript))]
+public class TestingUIEditor : Editor {
+
+	public override void OnInspectorGUI (){
+		DrawDefaultInspector ();
+		TestingUIScript UIScript = (TestingUIScript)target;
+
+		if (GUILayout.Button ("Empty")) {
+			UIScript.placingMode = TestingUIScript.TilePlacingMode.Empty;
+		}
+		if (GUILayout.Button ("Floor")) {
+			UIScript.placingMode = TestingUIScript.TilePlacingMode.Floor;
+		}
+		if (GUILayout.Button ("Wall")) {
+			UIScript.placingMode = TestingUIScript.TilePlacingMode.Wall;
+		}
+	}
+}
---
+++ MagickGame/Assets/_Scripts/TestingUI/TestingUIScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingUIScript : MonoBehaviour {
+
+	public enum TilePlacingMode {Empty,Floor,Wall};
+
+	public TilePlacingMode placingMode = TilePlacingMode.Empty;
+}
