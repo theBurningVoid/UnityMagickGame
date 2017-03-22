5cc136fb6dd6d43567f0e9a310756e604587e9eb
JRueckert582 <jrueckert582@gmail.com>
2017-03-21 23:26:32 -0500

    installed egoCS
    "installed" EgoCS framework so we can have a proper entity, component,
    system model
    -created gamecontroller gameobject and gave it an egointerface
    scriptcomponent

---
+++ MagickGame/Assets/EgoCS-master/Events/AddedComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class AddedComponent : EgoEvent{}
+
+public class AddedComponent<C> : AddedComponent
+    where C : Component
+{
+    public readonly C component;
+    public readonly EgoComponent egoComponent;
+
+    public AddedComponent( C component, EgoComponent egoComponent )
+    {
+        this.component = component;
+        this.egoComponent = egoComponent;
+    }
+}
---
+++ MagickGame/Assets/EgoCS-master/Events/DestroyedComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class DestroyedComponent : EgoEvent { }
+
+public class DestroyedComponent<C> : DestroyedComponent
+    where C : Component
+{
+    public readonly C component;
+    public readonly EgoComponent egoComponent;
+
+    public DestroyedComponent( C component, EgoComponent egoComponent )
+    {
+        this.component = component;
+        this.egoComponent = egoComponent;
+    }
+}
