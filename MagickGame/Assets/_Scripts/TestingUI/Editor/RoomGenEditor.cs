a10e5c97102f86082ef26ead6dfa6d0c0791c0f9
JRueckert582 <jrueckert582@gmail.com>
2017-03-24 03:14:37 -0500

    started RoomGenScript and RoomGenEditor
    created RoomGenScript monobehaviour script for converting rooms between
    the different
    wing types, as well as testing of room generation
    to be attached to the GameObject representing the room as a whole
    
    created RoomGenEditor to add 2 buttons to the inspector for
    RoomGenScript, Generate Room to call the GenerateRoom() method and Clear
    Room to call the ClearRoom() method

---
+++ MagickGame/Assets/_Scripts/TestingUI/Editor/RoomGenEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(RoomGenScript))]
+public class RoomGenEditor : Editor {
+
+	public override void OnInspectorGUI (){
+		DrawDefaultInspector ();
+		RoomGenScript genScript = (RoomGenScript)target;
+
+		if (GUILayout.Button ("Generate Room")) {
+			genScript.GenerateRoom ();
+		}
+		if (GUILayout.Button ("Clear Room")) {
+			genScript.ClearRoom ();
+		}
+	}
+}
---
+++ MagickGame/Assets/_Scripts/TestingUI/RoomGenScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//monobehaviour script for converting rooms between the different wing types, as well as testing of room generation
+//to be attached to the GameObject representing the room as a whole
+public class RoomGenScript : MonoBehaviour {
+
+	public enum WingType {
+		Alchemy,
+		Artifice,
+		Dungeon
+	}
+
+	//this exists because the subfolders dividing the room, tile, etc. 
+	//prefabs by wing type have the suffix "Wing" to clarify that they 
+	//are dividing by wing type not by magic type where needed (example:
+	//alchemy is both a type of magic and the name of a wing type)
+	public static string getPrefabResourcesPath(WingType wType)
+	{
+		return wType.ToString () + "Wing/";
+		//note the / which is used for Resources.Load instead of \
+		//which is normally used for file system paths
+	}
+	
+	public WingType wingType = WingType.Dungeon;
+
+	[Range(5,63)]
+	public int roomWidth = 5;
+	[Range(5,63)]
+	public int roomHeight = 5;
+
+
+	public void GenerateRoom () {
+		GameObject floorTile = Resources.Load("Tiles/" + getPrefabResourcesPath(wingType) + wingType.ToString() + "Floor") as GameObject;
+		GameObject wallTile = Resources.Load("Tiles/" + getPrefabResourcesPath(wingType) + wingType.ToString() + "Wall") as GameObject;
+		GameObject cornerWallTile = Resources.Load("Tiles/" + getPrefabResourcesPath(wingType) + wingType.ToString() + "Corner") as GameObject;
+
+		if (floorTile == null) {
+			Debug.Log ("floorTile is null, perhaps the asset has been moved/deleted or the path Tiles/" + wingType.ToString() + "Wing/ is incorrect?");
+		}
+		if (wallTile == null) {
+			Debug.Log ("wallTile is null, perhaps the asset has been moved/deleted or the path Tiles/" + wingType.ToString() + "Wing/ is incorrect?");
+		}
+		if (cornerWallTile == null) {
+			Debug.Log ("cornerWallTile is null, perhaps the asset has been moved/deleted or the path Tiles/" + wingType.ToString() + "Wing/ is incorrect?");
+		}
+		if (floorTile == null || wallTile == null || cornerWallTile == null) {
+
+		}
+
+	}
+
+	//removes all tiles and room defining components from this room to make it a blank or clear room
+	public void ClearRoom () {
+		MiscUtilities.DestroyImmediateAllChildren (this.transform);
+
+	}
+
+}
