e2de26e54d94b70b637c5c4fc2f370445fe17e96
Xechon <guess96@gmail.com>
2017-03-24 13:30:25 -0500

    Cleanup
    - Removed EntityFactory
    - Renamed TemplateUtilities to EntityFactoy
    - Removed unused PlayerController

--- MagickGame/Assets/_Scripts/Controllers/GameStateController.cs
+++ MagickGame/Assets/_Scripts/Controllers/GameStateController.cs
@@ -5,8 +5,8 @@ namespace Controllers {
 
 		// Use this for initialization
 		void Start() {
-			Ego.AddGameObject(TemplateEntities.GeneratePlayer().gameObject);
-			Ego.AddGameObject(TemplateEntities.GenerateGun().gameObject);
+			Ego.AddGameObject(EntityFactory.GeneratePlayer().gameObject);
+			Ego.AddGameObject(EntityFactory.GenerateGun().gameObject);
 			//Ego.AddGameObject(EntityFactory.generate ("Laser Gun", TemplateEntities.laserGun));
 			//EntityFactory.generate ("Zombie", TemplateEntities.zombie).SetActive (true);
 		}
--- MagickGame/Assets/_Scripts/EntityFactory.cs
+++ MagickGame/Assets/_Scripts/EntityFactory.cs
@@ -1,126 +1,65 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 using System;
-using System.Runtime.CompilerServices;
+using Assets._Scripts;
+using Components.Attachment.Event;
+using Components.Steering;
+using Attachments;
+using Components.Action;
+using Components.AI.Tree.Branch;
+using Components.AI.Tree.Leaf;
+using Components.AI.Tree.Selector;
+using Components.Attachment;
+using Components.Behavior_Tree;
+using Components.Steering.Event;
 
-/* Current rules for modifying component lists:
- * - Can't have more than one of the same component
- * - Superclasses are replaced with subclasses
- * - Archetypes cannot be modified, instead new ones are generated with desired modifications
- * - Non-archetype modifications are not supported
- */
-
-public static class EntityFactory
+public class EntityFactory
 {
-	public static GameObject generate(String name, params ComponentWrapper[] components) {
-		GameObject entity = new GameObject ();
-		foreach(ComponentWrapper wrapper in removeDuplicates(components)) {
-			Component comp = entity.AddComponent (wrapper.value);
-			if(wrapper.action != null) wrapper.action (comp);
-		}
-		entity.name = name;
-		//entity.SetActive (false);
+	public static EgoComponent GenerateBullet() {
+		return EntityBuilder.Generate().WithPhysics(typeof(CircleCollider2D), .2f).WithGraphics("Images/BaseTestingCircle").WithMotion();
+	}
+
+	public static EgoComponent GenerateGun() {
+		EntityBuilder entity = EntityBuilder.Generate().WithPhysics(typeof(BoxCollider2D), .5f).WithGraphics("Images/gun");
+		Interactive c = Ego.AddComponent<Interactive>(entity);
+		c.InteractAction = e => EgoEvents<AttachEvent>.AddEvent(new AttachEvent(c.GetComponent<EgoComponent>(), e));
+		Ego.AddComponent<Mountpoint>(entity);
+		Useable u = Ego.AddComponent<Useable>(entity);
+		u.UseAction = e => {
+			Transform transform = u.transform;
+			double theta = transform.rotation.eulerAngles.z*Mathf.Deg2Rad;
+			Vector2 force = new Vector2((float) Math.Cos(theta), (float) Math.Sin(theta));
+			force.Normalize();
+			// generate new projectile, add motion in direction at speed
+
+			EgoComponent bullet = Ego.AddGameObject(GenerateBullet().gameObject);
+			bullet.transform.rotation = transform.rotation;
+			bullet.transform.position = transform.position;
+			bullet.gameObject.SetActive(true);
+			EgoEvents<SetVelocityByEvent>.AddEvent(new SetVelocityByEvent(bullet, force));
+		};
 		return entity;
 	}
 
-	public static Archetype buildArchetype(params ComponentWrapper[] components) {
-		return new Archetype (components);
+	public static EgoComponent GenerateCreature() {
+		EntityBuilder entity = EntityBuilder.Generate().WithPhysics(typeof(CircleCollider2D)).WithMotion();
+		Ego.AddComponent<Hardpoint>(entity);
+		return entity;
 	}
 
-	static ComponentWrapper[] removeDuplicates(ComponentWrapper[] componenents) {
-		List<ComponentWrapper> keeps = new List<ComponentWrapper> ();
-		foreach (ComponentWrapper wrapper in componenents) {
-			if (keeps.FindAll (x => wrapper.value.IsAssignableFrom (x.value)).Count == 0) {
-				if (keeps.Remove (keeps.Find (x => x.value.IsAssignableFrom (wrapper.value)))) {
-					throw new Exceptions.ComponentListModifyException ("Superclass of type " + wrapper.value.ToString () +
-						" already found. Replacing superclass with subclass.");
-				}
-				keeps.Add (wrapper);
-			} else {
-				throw new Exceptions.ComponentListModifyException ("Component of type " + wrapper.value.ToString () +
-					" already found. Cannot add.");
-			}
-		}
-		return keeps.ToArray ();
-	}
+	public static EgoComponent GeneratePlayer() {
+		EntityBuilder entity = EntityBuilder.Generate().WithPhysics(typeof(CircleCollider2D)).WithMotion().WithGraphics("Images/player");
+		Ego.AddComponent<MovementInput>(entity);
+		Ego.AddComponent<Hardpoint>(entity);
+		Ego.AddComponent<BehaviorTree>(entity).TrunkNode = 
+		new SelectorAll(
+			new ButtonPressed(
+				new Drop(), "Drop"),
+			new ButtonPressed(
+				new Interact(), "Interact"),
+			new ButtonPressed(
+				new Use(), "Fire1")
+			);
 
-	public class Archetype {
-		private List<ComponentWrapper> components;
-
-		public Archetype(params ComponentWrapper[] components) {
-			this.components = new List<ComponentWrapper>();
-			this.components.AddRange(removeDuplicates(components));
-		}
-
-		/* Returns a new archetype that contains this archetype's components, 
-		 * in addition to the ones in the given components as long as there aren't any 
-		 * instances of them in the component list already.
-		 */
-		public Archetype add(params ComponentWrapper[] components) {
-			List<ComponentWrapper> list = new List<ComponentWrapper> ();
-			list.AddRange (this.components);
-			foreach (ComponentWrapper wrapper in components) {
-				if (list.FindAll (x => x.value.IsAssignableFrom (wrapper.value)).Count == 0) {
-					list.Add (wrapper);
-				} else 
-					throw new Exceptions.ComponentListModifyException("Component of type " + wrapper.value.ToString () + 
-						" already found. Cannot add.");
-			}
-			return new Archetype (list.ToArray ());
-		}
-
-		/* Returns a new archetype that contains this archetype's components, 
-		 * except for the ones that are instances of the given components. Disregards actions.
-		 */
-		public Archetype remove(params ComponentWrapper[] components) {
-			List<ComponentWrapper> list = new List<ComponentWrapper> ();
-			list.AddRange (this.components);
-			foreach(ComponentWrapper wrapper in components) {
-				if (list.RemoveAll (x => x.value.IsAssignableFrom (wrapper.value)) == 0) {
-					throw new Exceptions.ComponentListModifyException ("No components of type " + wrapper.value.ToString () + 
-						" found. Cannot remove.");
-				}
-			}
-			return new Archetype (list.ToArray ());
-		}
-
-		/* Returns a new archetype that contains this archetype's components,
-		 * except with the given componenets replacing those that it is an instance of.
-		 */
-		public Archetype replace(params ComponentWrapper[] components) {
-			List<ComponentWrapper> list = new List<ComponentWrapper> ();
-			list.AddRange (this.components);
-			foreach(ComponentWrapper wrapper in components) {
-				List<ComponentWrapper> matches = list.FindAll (x => wrapper.value.IsAssignableFrom (x.value));
-				if (matches.Count == 0)
-					throw new Exceptions.ComponentListModifyException  ("No components of type " + wrapper.value.ToString () + 
-						" found. Cannot replace.");
-				else {
-					foreach (ComponentWrapper comp in matches) {
-						list.Remove (comp);
-					}
-					list.Add (wrapper);
-				}
-			}
-			return new Archetype (list.ToArray ());
-		}
-
-		public static implicit operator ComponentWrapper[](Archetype a) {
-			return a.components.ToArray ();
-		}
-	}
-}
-
-public struct ComponentWrapper {
-	public Type value;
-	public Action<Component> action;
-
-	public ComponentWrapper(Type value, Action<Component> action) {
-		this.value = value;
-		this.action = action;
-	}
-
-	public static implicit operator ComponentWrapper(Type t) {
-		return new ComponentWrapper (t, null);
+		return entity;
 	}
 }
